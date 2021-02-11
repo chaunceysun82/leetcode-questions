058981e1acc0eb1e0e7b40a996db44360ac66f41
wwweiwang <35875872+wwweiwang@users.noreply.github.com>
2021-02-11 12:30:20 -0800

    update 937

--- Wei_code/937_Reorder_Data_in_Log_Files.py
+++ Wei_code/937_Reorder_Data_in_Log_Files.py
@@ -1,5 +1,6 @@
 logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
 
+#create 2 lists. one for letters and the other for digitals
 letter_list = []
 digit_list = []
 
@@ -9,5 +10,7 @@ for log in logs:
     else:
         letter_list.append(log)
 
+#sort letter logs
 sorted_letter_list = sorted(letter_list, key = lambda log: (log.split()[1:], log.split()[0]))
+
 print(sorted_letter_list + digit_list)
