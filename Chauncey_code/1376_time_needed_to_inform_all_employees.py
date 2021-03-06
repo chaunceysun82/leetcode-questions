dc7aa6293ad5b8ec3785fa192801de46dc21fac2
chaunceysun82 <chauncey.sun1982@gmail.com>
2021-03-06 11:47:18 -0800

    commit 100, 110. 1376

--- Chauncey_code/1376_time_needed_to_inform_all_employees.py
+++ Chauncey_code/1376_time_needed_to_inform_all_employees.py
@@ -1,33 +1,28 @@
-import collections
+from collections import deque, defaultdict
 
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager, informTime) -> int:
-        structure = {id : [] for id in range(n)}
+
+        structure = defaultdict(list)
         for id, manager_id in enumerate(manager):
             if manager_id != -1:
-                structure[manager_id].append(id)
-        
-        print(structure)
+                structure[manager_id].append((id, informTime[id]))
 
-        queue = collections.deque([headID])
-        total_time_needed = 0
+        queue = deque([(headID, informTime[headID])])
+        max_time = 0
 
-        while len(queue) > 0:
-            employee_num = len(queue)
-            time_needed = 0
-            for _ in range(employee_num):
-                employee_id = queue.popleft()
-                time_needed = max(time_needed, informTime[employee_id])
+        while queue:
+            employee_id, current_time = queue.popleft()
 
-                if not structure[employee_id]:
-                    continue
+            if not structure[employee_id]:
+                continue
 
-                for subordinate_id in structure[employee_id]:
-                    queue.append(subordinate_id)
+            max_time = max(max_time, current_time)
 
-            total_time_needed += time_needed
+            for subordinate in structure[employee_id]:
+                queue.append((subordinate[0], subordinate[1] + current_time))
 
-        return total_time_needed
+        return max_time
 
 
 n = 11
@@ -37,3 +32,4 @@ informTime = [0,213,0,253,686,170,975,0,261,309,337]
 
 obj = Solution()
 print(obj.numOfMinutes(n, headID, manager, informTime))
+
